0d62e02b890a3af609a8796fdb619981f804bcd5
Liam Stabeno and Shane Stafford <liam@>
2017-02-27 14:28:22 -0800

    add functionality to store individual list items and export as complete list

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+//Back-end logic
+function List(task) {
+  this.task = task
+};
+List.prototype.addTask = function() {
+  return this.task;
+};
+var listArray = [];
+
+//Front-end logic
+$(function() {
+  $("#formOne").submit(function() {
+    event.preventDefault();
+    var inputtedTask = $("#task-list").val();
+
+    var newList = new List(inputtedTask);
+
+    $("ul#lists").append("<li><span class='list'>" + newList.addTask() + "</span></li>")
+    listArray.push(newList.addTask());
+    //   $(".list").last().click(function() {
+    //     $("#show-list ul").append(newList.text);
+    // });
+  });
+  $("#formTwo").submit(function() {
+    event.preventDefault();
+    for (i = 0; i < listArray.length; i++)
+    {
+      $("ul#show-list").append("<li><span id='hideMe'>" + listArray[i] + " " + "</span></li>");
+    }
+
+    $("#hideMe").click(function() {
+        $("#hideMe").hide();
+      });
+
+  });
+});
